98eac477fd98dc04debbfda0d753f0a76b5ae148
Yuan <talentedyuan@163.com>
2018-09-05 21:40:18 +0800

    常量工具类

--- app/src/main/java/com/example/astronaut/brain_dots/Utils/Constant.java
+++ app/src/main/java/com/example/astronaut/brain_dots/Utils/Constant.java
@@ -3,11 +3,19 @@ package com.example.astronaut.brain_dots.Utils;
 import com.example.astronaut.brain_dots.Utils.screenManagerUtils.ScreenScaleResult;
 import com.example.astronaut.brain_dots.Utils.screenManagerUtils.ScreenScaleUtil;
 
+import java.io.Serializable;
+
 
 /**
  * 该类定义的常量用于各种计算
  */
 public class Constant {
+    //表示在onActivityResult()中更新金钱的数量的返回值(在)
+    public final static int REQUEST_REFRESH_GOLD_NUM_CODE = 1001;
+    //花费金币后 更新金钱数量的Handler标记
+    public static final int SUB_GOLD_NUM_HANDLER = 10;
+    //增加金币后 更新金钱数量的Handler标记
+    public static final int ADD_GOLD_NUM_HANDLER = 11;
     /*
      * 该常量用于传递给Handler的消息,而内置的what变量可以为整型常量
      * 要用final来修饰该变量
@@ -16,6 +24,11 @@ public class Constant {
      *
      * */
 
+    /*
+     * 表示游戏金币的数量
+     *
+     * */
+    public static int MONEY = 100;
     public static final int HANDLER_MSG_LOSE = 0;
     public static final int HANDLER_MSG_PASS = 1;
     /*
@@ -37,9 +50,10 @@ public class Constant {
     /*
      * 告诉Activity进入加载界面的Handler标志
      * */
-    public static final int START_LOADING = 4;
+    public static final int NEXT_LEVEL = 4;
     //重新开始这关卡游戏的Handler标志
     public static final int RENEW_GAME = 5;
+    public static boolean IS_NEW_THREAD = true;
     //是否开启世界模拟的标志
     public static boolean DRAW_THREAD_FLAG = true;
     public static final float TIME_STEP = 2.0f / 60.0f;//模拟的的频率
@@ -48,11 +62,9 @@ public class Constant {
     public static int SCREEN_HEIGHT = 1280;//目标屏幕高度
     public static float x;//声明当前屏幕左上方X值的变量
     public static float y;//声明当前屏幕左上方Y值的变量
-    
+
     private static float ratio;//声明缩放比例的变量
 
-    public static String CURRENT_LEVEL = null;
-    public static String NEXT_LEVEL = null;
     //单位矩形的长和宽
     public static final int RECTANGLE_WIDTH = 100;
     public static final int RECTANGLE_HEIGHT = 10;
